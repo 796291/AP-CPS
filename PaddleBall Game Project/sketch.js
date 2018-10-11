849882497378ca865ba6692fb93385d0ddd84ec9
796291 <43755477+796291@users.noreply.github.com>
2018-10-11 08:49:59 -0700

    added splice to buttom paddle

--- PaddleBall Game Project/sketch.js
+++ PaddleBall Game Project/sketch.js
@@ -14,11 +14,13 @@ function setup(){
   //creating the lerping paddle
   //
   var loc = createVector(400, 550)
+  var loc2 = createVector(400, 560)
   var vel = createVector(0, 0);
   var width = 250;
   var length = 20;
   var col = color(random(0, 255), random(0, 255), random(0, 255))
-  paddle = new Paddle(loc, vel, width, length, col)
+  paddle = new Paddle(loc, vel, width, length/2, col);
+  paddle2 = new Paddle(loc2, vel, width, length/2, col);
 }
 //
 //load balls
@@ -43,19 +45,24 @@ function draw(){
   //get rid of outlines
   noStroke();
   paddle.run();
+  paddle2.run();
   for(var i = 0; i < Balls.length; i++){
     Balls[i].run();
     //make balls bounce off the paddle
     var aBalls = Balls[i]
     var y = aBalls.loc.y
     var x = aBalls.loc.x
-    //distance between paddle and ball in y coords
-    var distY = abs(paddle.loc.y - y);
+    //distance between paddle1 and ball in y coords
+    var distY1 = y - paddle.loc.y;
+    var distY2 = y - paddle2.loc.y;
     //needs to be within y distance and within the paddles 2 corners
-    if((distY < 20) && (x > paddle.loc.x) && (x < paddle.loc.x+250)){
+    if((distY1 > -10) && (distY1 < 0) && (x > paddle.loc.x) && (x < paddle.loc.x+250)){
       aBalls.vel.y = -aBalls.vel.y
+    }
+    //splice out the balls when they hits paddle2 (make sure to keep them in for paddle1 hits
+    if((distY2 < 10) && (distY2 > 0) && (x > paddle.loc.x) && (x < paddle.loc.x+250)){
+      Balls.splice(i,1);
     }
   }
 }
-
 //score
