dbfb10d2726d44b4fd447eff6cf1ac387d0b8716
796291 <43755477+796291@users.noreply.github.com>
2018-10-23 08:49:44 -0700

    words start

--- Lab1022 WordSort/sketch.js
+++ Lab1022 WordSort/sketch.js
@@ -1,16 +1,25 @@
 
-//  This is a comment
-//  The setup function function is called once when your program begins
+//Jakob Hachigian-Kreutzer
+//Lab1022 WordSort
+
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
-
-
 }
 
-//  The draw function is called @ 30 fps
+
 function draw() {
+}
+
+function checkAlphabet(){
+  nums.
+  alphabet = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
+  for(var i = words.length - 1; i >= 1; i--){
+    for(var j = 0; j < words.length; j++){
+
+    }
+  }
 
 }
