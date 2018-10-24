880509f02994fd94f44b2cb8d08db09d6a814785
796291 <43755477+796291@users.noreply.github.com>
2018-10-23 22:50:19 -0700

    fixed bubble sort
    --can sort from an array, need to make it set the file as an array
    --can only sort by 1st letter of string currently

--- Lab1022 WordSort/sketch.js
+++ Lab1022 WordSort/sketch.js
@@ -1,25 +1,41 @@
 
 //Jakob Hachigian-Kreutzer
 //Lab1022 WordSort
-
 function setup() {
-  var cnv = createCanvas(800, 800);
-  cnv.position((windowWidth-width)/2, 30);
-  background(5, 5, 5);
-  fill(200, 30, 150);
+  //remove blank black screen
+  noCanvas();
 }
-
 
 function draw() {
+  //call sorting function
+  bubbleSort(txt);
 }
-
-function checkAlphabet(){
-  nums.
-  alphabet = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
-  for(var i = words.length - 1; i >= 1; i--){
-    for(var j = 0; j < words.length; j++){
-
+//what is being sorted
+var txt = ["bat", "cat", "rat", "scum", "rum", "food", "dude"];
+//print original text
+console.log(txt);
+function bubbleSort(txt) {
+    var length = txt.length;
+    //number of passes
+    for (var i = 0; i < length; i++) {
+        //notice that j < (length - i)
+        for (var j = 0; j < (length - i - 1); j++) {
+            //Compare the adjacent positions
+            var az = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+            //look at the 1st letters of the 2 words being compared
+            var txt1 = az.indexOf(txt[j].charAt(0));
+            var txt2 = az.indexOf(txt[j+1].charAt(0));
+            if(txt1 > txt2) {
+                //swap the numbers
+                //temperary variable
+                var temp = txt[j];
+                //replace current string with adjacent string
+                txt[j] = txt[j+1];
+                //replace adjacent string with temp string (current string)
+                txt[j+1] = temp;
+                //print sorting progress
+                console.log(txt);
+            }
+        }
     }
-  }
-
 }
