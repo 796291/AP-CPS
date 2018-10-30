5d535c5f3ebbffd3ab8312b1359766e1b20ecb74
796291 <43755477+796291@users.noreply.github.com>
2018-10-29 22:40:22 -0700

    added the color bar

--- Lab1029 Color Bars/sketch.js
+++ Lab1029 Color Bars/sketch.js
@@ -1,24 +1,50 @@
 
 //Jakob Hachigian-Kreutzer
 //Lab1029 Color Bars
-var bars = [];
+var Bars = [];
 
 function setup() {
-  bubbleSort(bars);
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(20, 20, 20);
+  numBars = 40;
+  loadBars(numBars);
+  bubbleSort(Bars);
 }
 
 function draw() {
+  background(20, 20, 20, 6000);
+  noStroke();
+  for(var i = 0; i < Bars.length; i++){
+    Bars[i].run();
+  }
 }
 
-function bubbleSort(bars) {
-    var length = bar.length;
+function loadBars(numBars){
+  for(var i = 0; i < numBars; i++){
+    var w = 800/numBars;
+    var h = 250;
+    var loc = createVector(0+(w*i), 150);
+    var red = random(0, 255);
+    var green = random(0, 255);
+    var blue = random(0, 255);
+    var clr = color(red, green, blue);
+    var clrAvg = (red + green+ blue)/3;
+    var bar = new colorbar(loc, w, h, clr, clrAvg);
+    Bars.push(bar);
+  }
+}
+
+function bubbleSort(Bars) {
+    var length = Bars.length;
     for (var i = 0; i < length; i++) {
       for (var j = 0; j < (length - i - 1); j++) {
-        bars[j].clrAvg
-        if(bar[j] > bar[j+1]) {
-          var temp = bar[j];
-          bar[j] = bar[j+1];
-          bar[j+1] = temp;
+        var a = Bars[j].clrAvg;
+        var b = Bars[j+1].clrAvg;
+        if(a > b) {
+          var temp = Bars[j];
+          Bars[j] = Bars[j+1];
+          Bars[j+1] = temp;
             }
         }
     }
