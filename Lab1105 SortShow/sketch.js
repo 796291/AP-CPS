ac53248d9bded8daefcf205712248e20662a1c38
796291 <43755477+796291@users.noreply.github.com>
2018-11-13 01:04:48 -0800

    progress into pie chart creation
    total works
    pie chart itself does not
    ---ANGLES need to be fixed somehow

--- Lab1105 SortShow/sketch.js
+++ Lab1105 SortShow/sketch.js
@@ -5,6 +5,7 @@
 var diameter = 400;
 var angles = [];
 var data = [];
+var total = 0
 function preload(){
   data = loadJSON("population.json");
 }
@@ -14,9 +15,11 @@ function setup() {
   cnv.position((windowWidth-width)/2, 30);
   background(20, 20, 20, 20);
   bubbleSort(data);
+  getTotal();
 }
 
 function draw() {
+  pieChart(400, data);
 }
 
 function bubbleSort(data) {
@@ -27,29 +30,38 @@ function bubbleSort(data) {
       //notice that j < (length - i)
       for (var j = 0; j < (length - i - 1); j++) {
           //Compare the adjacent positions
-          var a = data.countrydata.total;
-          var b = data.countrydata.total;
+          var a = data.countrydata[j].total;
+          var b = data.countrydata[j+1].total;
           if(a > b) {
               //swap the numbers
               //temperary variable
-              var temp = data[j];
+              var temp = data.countrydata[j];
               //replace current string with adjacent string
-              data[j] = data[j+1];
+              data.countrydata[j] = data.countrydata[j+1];
               //replace adjacent string with temp string (current string)
-              data[j+1] = temp;
+              data.countrydata[j+1] = temp;
             }
         }
     }
 }
 
 
+function getTotal(){
+  length = data.countrydata.length
+  for (var i = 0; i < length; i++){
+    total = total + data.countrydata[i].total;
+  }
+  console.log(total)
+}
 
-//function pieChart(diameter, data){
-//  var lastAngle = 0;
-//  for (var i = 0; i < data.length, 0, 255){
-//    var color = map(i, 0. data.length, 0, 255);
-//    fill()
-//    arc(400, 400, diameter, diameter, lastAngle, lastAngle + radians(angles[i]));
-//    lastAngle += radians(angles[i]);
-//  }
-//}
+
+//PROGRESS
+function pieChart(diameter, data){
+  length = data.countrydata.length
+  for (var i = 0; i < length; i++){
+    var gray = map(i, 0, data.countrydata.length, 0, 255);
+    fill(gray);
+    var angle = (2*(PI)*(data.countrydata.total))/total
+    arc(400, 400, diameter, diameter, 0, angle);
+  }
+}
