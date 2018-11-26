fc66df12aebbc303ca3854bb8991867148abcf02
796291 <43755477+796291@users.noreply.github.com>
2018-11-26 08:49:54 -0800

    keycodes

--- Snake Game/sketch.js
+++ Snake Game/sketch.js
@@ -1,6 +1,7 @@
 var w = 20;
 var cols, rows;
 var snake;
+var segments = [];
 
 function setup(){
   frameRate(10);
@@ -22,21 +23,37 @@ function draw(){
 function keyPressed(){
   //up
   if(keyCode === 38){
+    for(var i = 0; i < numSegment - 1; i++){
+      segments[0].loc = snake.loc
+      segments[i].loc = segments[i - 1].loc
+    }
     snake.vel = createVector(0, -20);
     snake.loc.add(snake.vel);
   }
   //down
   if(keyCode === 40){
+    for(var i = 0; i < numSegment - 1; i++){
+      segments[0].loc = snake.loc
+      segments[i].loc = segments[i - 1].loc
+    }
     snake.vel = createVector(0, 20);
     snake.loc.add(snake.vel);
   }
   //right
   if(keyCode === 39){
+    for(var i = 0; i < numSegment - 1; i++){
+      segments[0].loc = snake.loc
+      segments[i].loc = segments[i - 1].loc
+    }
     snake.vel = createVector(20, 0);
     snake.loc.add(snake.vel);
   }
   //left
   if(keyCode === 37){
+    for(var i = 0; i < numSegment - 1; i++){
+      segments[0].loc = snake.loc
+      segments[i].loc = segments[i - 1].loc
+    }
     snake.vel = createVector(-20, 0);
     snake.loc.add(snake.vel);
   }
