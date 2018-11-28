c9d856a9e7bfe2689ddd0ec381b33c51d03f405c
796291 <43755477+796291@users.noreply.github.com>
2018-11-28 03:22:31 -0800

    massive overhaul

--- Snake Game/sketch.js
+++ Snake Game/sketch.js
@@ -1,60 +1,73 @@
-var w = 20;
-var cols, rows;
-var snake;
-var segments = [];
+var snake = [];
+var food = [];
+var s;
 
 function setup(){
-  frameRate(10);
+  frameRate(15);
   var cnv = createCanvas(800, 800);
-  cnv.position(windowWidth-width)/(2, 30);
+  cnv.position((windowWidth-width)/2, 30);
   background(20, 20, 20);
-  cols = width/w;
-  rows = height/w;
-  var loc = createVector(width/2, height/2);
-  var vel = createVector(1, 0);
-  snake = new Snake(loc, vel);
+  loadSnake(1);
+  loadFood(1);
 }
 
 function draw(){
-  background(20, 20, 20, 6000);
-  snake.run();
+  background(20, 20, 20);
+
+  for(var i = 0; i < snake.length; i++){
+    snake[i].run();
+  }
+
+  for(var i = 0; i < food.length; i++){
+    food[i].run();
+  }
+
+  checkLoc();
+}
+
+function checkLoc(){
+  for(var i = 0; i < food.length; i++){
+    var distX = food[i].loc.x - snake[0].loc.x;
+    var distY = food[i].loc.y - snake[0].loc.y;
+    if(distX == (0) && distY == (0)){
+      food.splice(i, 1);
+      loadFood(1);
+    }
+  }
+}
+
+function loadSnake(numSnake){
+  for(var i = 0; i < numSnake; i++){
+    var loc = createVector(200, 200);
+    var vel = createVector(0, 0);
+    s = new Snake(loc, vel);
+    snake.push(s);
+  }
+}
+
+function loadFood(numFood){
+  for(var i = 0; i < numFood; i++){
+    var min = 1;
+    var max = 40;
+    var locX = (Math.floor(Math.random() * (max - min + 1) + min)) * 20;
+    var locY = (Math.floor(Math.random() * (max - min + 1) + min)) * 20;
+    var loc = createVector(locX, locY);
+    var f = new Food(loc);
+    food.push(f);
+  }
 }
 
 function keyPressed(){
-  //up
   if(keyCode === 38){
-    for(var i = 0; i < numSegment - 1; i++){
-      segments[0].loc = snake.loc
-      segments[i].loc = segments[i - 1].loc
-    }
-    snake.vel = createVector(0, -20);
-    snake.loc.add(snake.vel);
+    snake[0].vel = createVector(0, -20)
   }
-  //down
   if(keyCode === 40){
-    for(var i = 0; i < numSegment - 1; i++){
-      segments[0].loc = snake.loc
-      segments[i].loc = segments[i - 1].loc
-    }
-    snake.vel = createVector(0, 20);
-    snake.loc.add(snake.vel);
+    snake[0].vel = createVector(0, 20)
   }
-  //right
   if(keyCode === 39){
-    for(var i = 0; i < numSegment - 1; i++){
-      segments[0].loc = snake.loc
-      segments[i].loc = segments[i - 1].loc
-    }
-    snake.vel = createVector(20, 0);
-    snake.loc.add(snake.vel);
+    snake[0].vel = createVector(20, 0)
   }
-  //left
   if(keyCode === 37){
-    for(var i = 0; i < numSegment - 1; i++){
-      segments[0].loc = snake.loc
-      segments[i].loc = segments[i - 1].loc
-    }
-    snake.vel = createVector(-20, 0);
-    snake.loc.add(snake.vel);
+    snake[0].vel = createVector(-20, 0)
   }
 }
--- Snake Game/snake.js
+++ Snake Game/snake.js
@@ -1,8 +1,7 @@
-function Snake(loc, vel, status){
+function Snake(loc, vel){
 
   this.loc = loc;
   this.vel = vel;
-  this.status = status
 
   this.run = function(){
     this.update();
@@ -11,38 +10,14 @@ function Snake(loc, vel, status){
 
   this.update = function(){
     this.loc.add(this.vel);
-    this.loc.x = constrain(this.loc.x, 0, width-w)
-    if(this.loc.x = constrain(this.loc.x, 0, width-w)){
-
-    }
-    this.loc.y = constrain(this.loc.y, 0, width-w)
-    if(this.loc.y = constrain(this.loc.y, 0, width-w)){
-
-    }
-
+    this.loc.x = constrain(this.loc.x, 0, 800-20)
+    this.loc.y = constrain(this.loc.y, 0, 800-20)
   }
 
   this.render = function(){
-    fill(255, 255, 255);
-    rect(this.loc.x, this.loc.y, w, w);
+    noStroke();
+    fill(0, 255, 0);
+    rect(this.loc.x, this.loc.y, 20, 20);
   }
 
 }
-//snake code:
-
-//keep track of location, velocity, color
-//update function
-  //move the snake a certain amount
-    //direction of velocity after key is pressed = direction of movement
-    //need to move by a width (like within a grid)
-  //stop the snake when it touches a wall
-    //return "status" = true or false to restart the game if it touches the wall
-//render function
-  //gives snake a color
-  //creates the rectangle for the snake
-    //width and height = w (20), one square in the grid
-//tangled function
-  //checks if the snake is "tangled"
-    //if a segment is running into another segment/head of the snake
-  //returns "status" true or false
-    //can be used to decide to start a new game or not
