8946d7b0eb6717b40cf9ff554bd6f7087c1a49f5
796291 <43755477+796291@users.noreply.github.com>
2018-11-29 04:57:51 -0800

    added segments

--- Snake Game/sketch.js
+++ Snake Game/sketch.js
@@ -1,22 +1,19 @@
-var snake = [];
+var snake;
 var food = [];
-var numSnake = 1;
+var numSeg = 1;
 
 function setup(){
-  frameRate(15);
+  frameRate(10);
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(20, 20, 20);
-  loadSnake(numSnake);
+  loadSnake();
   loadFood(1);
 }
 
 function draw(){
   background(20, 20, 20);
-
-  for(var i = 0; i < snake.length; i++){
-    snake[i].run();
-  }
+  snake.run();
 
   for(var i = 0; i < food.length; i++){
     food[i].run();
@@ -27,36 +24,20 @@ function draw(){
 
 function checkLoc(){
   for(var i = 0; i < food.length; i++){
-    var distX = food[i].loc.x - snake[0].loc.x;
-    var distY = food[i].loc.y - snake[0].loc.y;
+    var distX = food[i].loc.x - snake.loc.x;
+    var distY = food[i].loc.y - snake.loc.y;
     if(distX == (0) && distY == (0)){
       food.splice(i, 1);
       loadFood(1);
-      numSnake = numSnake + 1
+      snake.numSeg = snake.numSeg + 1;
     }
   }
 }
 
-function loadSeg(numSeg){
-  for(var i = 0; i < numSeg; i++){
-
-  }
-}
-
-function loadSnake(numSnake){
-  if(numSnake = 1){
-    var loc = createVector(200, 200);
-    var vel = createVector(0, 0);
-    s = new Snake(loc, vel);
-    snake.push(s);
-  }else if(numSnake > 1){
-    for(var i = 1; i < numSnake; i++){
-      var loc = createVector(snake[i-1].loc.x, snake[i-1].loc.y)
-      var vel = createVector(0, 0);
-      seg = new Snake(loc, vel);
-      snake.push()
-    }
-  }
+function loadSnake(){
+  var loc = createVector(200, 200);
+  var vel = createVector(0, 0);
+  snake = new Snake(loc, vel, numSeg);
 }
 
 function loadFood(numFood){
@@ -74,15 +55,15 @@ function loadFood(numFood){
 
 function keyPressed(){
   if(keyCode === 38){
-    snake[0].vel = createVector(0, -20)
+    snake.vel = createVector(0, -20)
   }
   if(keyCode === 40){
-    snake[0].vel = createVector(0, 20)
+    snake.vel = createVector(0, 20)
   }
   if(keyCode === 39){
-    snake[0].vel = createVector(20, 0)
+    snake.vel = createVector(20, 0)
   }
   if(keyCode === 37){
-    snake[0].vel = createVector(-20, 0)
+    snake.vel = createVector(-20, 0)
   }
 }
--- Snake Game/snake.js
+++ Snake Game/snake.js
@@ -1,7 +1,10 @@
-function Snake(loc, vel){
+function Snake(loc, vel, numSeg){
 
   this.loc = loc;
   this.vel = vel;
+  this.numSeg = numSeg;
+  this.segments = [];
+  this.segments.push(createVector(0, 0));
 
   this.run = function(){
     this.update();
@@ -9,12 +12,20 @@ function Snake(loc, vel){
   }
 
   this.update = function(){
+    this.segments[0].x = this.loc.x;
+    this.segments[0].y = this.loc.y;
     this.loc.add(this.vel);
     this.loc.x = constrain(this.loc.x, 0, 800-20)
     this.loc.y = constrain(this.loc.y, 0, 800-20)
   }
 
   this.render = function(){
+    for(var i = 0; i < this.numSeg; i++){
+      noStroke();
+      fill(0, 0, 255);
+      this.segments.push(createVector(this.segments[i].x, this.segments[i].y));
+      rect(this.segments[i].x, this.segments[i].y, 20, 20)
+    }
     noStroke();
     fill(0, 255, 0);
     rect(this.loc.x, this.loc.y, 20, 20);
